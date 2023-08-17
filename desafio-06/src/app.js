35b5e9e6ab73a97aa7f36cb5a8b96f4da006b7c7
GonzaCC <acanazac@uni.pe>
2023-08-17 16:42:23 -0500

    Improved Challenge 06

--- desafio-06/src/app.js
+++ desafio-06/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
 app.get("/", (req, res) => {
-    res.send("Utilizando express")
+    res.send("Welcome Challenge 06")
 })
 
 app.get("/products", async (req, res) => {
